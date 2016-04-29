c6ab2a05b13a0812ac54bab7affa32f3f37cf554
Rigo85 <rlsalgado@uci.cu>
2016-04-29 13:42:36 -0400

    initial commit

---
+++ src/main/java/org/jhfs/mvc/jHFSApp.java
@@ -0,0 +1,18 @@
+package org.jhfs.mvc;
+
+/**
+ * Author Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
+ * <p>
+ * Copyright 2016 by Rigoberto Leander Salgado Reyes.
+ * <p>
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http:www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ */
+public class jHFSApp {
+    public static void main(String... args) {
+        System.out.println("adadadadasd");
+    }
+}
